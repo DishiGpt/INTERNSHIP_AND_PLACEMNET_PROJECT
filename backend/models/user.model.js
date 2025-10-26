c3683d1a96f34dc45c285e928afb816d854c4b47
DishiGpt <dishigpt15@gmail.com>
2025-10-27 01:53:10 +0530

    Initial commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Mongoose } from "mongoose";
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,},
+    PhoneNumber:{
+        type:Number,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:["student","recruiter"],
+        required:true,
+    },
+    profile:{
+        bio:{type:String},
+        skills:{type:String},
+        resume:{type:String}, //url to resume file
+        resumeOriginalName:{type:String},
+        company:{type:Mongoose.Schema.Types.ObjectId, ref:"Company"}, //for recruiter
+        profilePhoto:{type:String,
+            default:""
+        },
+    }
+},{timestamps:true});
+export const User= mongoose.model("User",userSchema);
