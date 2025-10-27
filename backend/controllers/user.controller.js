12c3b327d2cd2398eb9a50a81de3678df23f721e
DishiGpt <dishigpt15@gmail.com>
2025-10-27 11:06:55 +0530

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/user.model.js";
-
+import jwt from "jsonwebtoken";  
 export const register = async (req, res) => {
     try{
         const {fullname, email, phoneNumber, password, role} = req.body;
@@ -27,8 +27,89 @@ export const register = async (req, res) => {
             password:hashedPassword,
             role     
         });
+
+        return res.status(201).json({
+            message:"User registered successfully",
+            success:true
+        });
     }
     catch(error){
         res.status(500).json({message:error.message});
     }
 };
+export const login = async (req, res) => {
+    try{
+        const {email, password,role} = req.body;
+        if(!email || !password || !role){  
+            return res.status(400).json(
+                {message:"All fields are required",
+                success:false
+                }
+            );
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json(
+                {message:"incorrect email or password",
+                success:false
+                }
+            );
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json(
+                {message:"incorrect email or password",
+                success:false
+                }
+            );
+        }
+        if(role !== user.role){
+            return res.status(400).json(
+                {message:"Account with current role doesn't exist.",
+                success:false
+                }
+            );
+        }
+        const tokenData={
+            userID:user._id,
+        }
+        const token = jwt.sign(tokenData,process.env.SECRET_KEY ,{expiresIn:'1d'}); 
+
+        user={
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,   
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile:user.profile, 
+        }
+
+        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpsOnly:true,sameSite:'strict'}).json({
+            message:`Welcome back! ${user.fullname}`,
+            success:true,
+        })
+    }
+catch(error){
+    console.log(error);
+}
+}
+
+export const logout = async (req, res) => {
+    try{
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"Logged out successfully",
+            success:true,
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+export const updateProfile = async (req, res) => {
+    try{
+
+    }
+    catch (error){
+        console.log(error);
+    }
+}
